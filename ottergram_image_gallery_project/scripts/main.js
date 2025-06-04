1f4d9f7ba3a5feb6c957747b55339a5283a4eb4e
Muki <mukidev881@gmail.com>
2025-06-04 19:19:53 +0200

    Handling Events With Javascript

---
+++ ottergram_image_gallery_project/scripts/main.js
@@ -0,0 +1,51 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_IMAGE_TITLE = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetais(imageUrl,titleText){
+    'use strict';
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src',imageUrl);
+
+    var detailTitle = document.querySelector(DETAIL_IMAGE_TITLE);
+    detailTitle.textContent = titleText;
+
+}
+
+function imageFromThumb(thumbnail){
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail){
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail){
+    'use strict';
+    setDetais(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThunbClickHandler(thumb){
+    'use strict';
+    thumb.addEventListener('click', function(event){
+        event.preventDefault();
+        setDetailsFromThumb(thumb)
+    })
+}
+
+function getThumbnailsArray(){
+    'use strict';
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+}
+
+function initializeEvents(){
+    'use strict';
+    var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThunbClickHandler)
+}
+
+initializeEvents();
